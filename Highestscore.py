e578a2c3233e4325e88865c6a3277d2a2de3a77b
jayslick2025 <jayslick2295@gmail.com>
2025-09-22 21:04:04 +0100

    Add files via upload

---
+++ Highestscore.py
@@ -0,0 +1,15 @@
+First number = int(input('Enter first score:'))
+Second number=  int(input('Enter second score:'))
+Third number=  int(input('Enter third score:'))
+
+highest = first number
+if highest < Second number :
+      
+        highest = Second number        
+ 
+if highest < Third number : 
+         print('highest score is', Third number)
+
+else : 
+         print('highest score is', Second number) 
+
