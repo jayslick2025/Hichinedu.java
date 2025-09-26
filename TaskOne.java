030f273c431942c09908b5097154393c1ba815f8
jayslick2025 <jayslick2295@gmail.com>
2025-09-26 11:59:21 +0100

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,32 @@
+
+
+
+public class TaskOne{
+public static void main(String [] args){
+
+/*int count = 1;
+
+System.out.println("count");
+*/
+
+for(; count <= 10; count+1)
+{
+
+
+if(number > 10);
+    count = -1;
+
+
+
+
+  { System.out.println("count");
+
+
+         }
+
+       }
+
+    }
+
+  }                                                                                           
+
