8f0a9f76c4d75e98f2152cb4ec85c98c130eb31e
jayslick2025 <jayslick2295@gmail.com>
2025-09-26 12:12:37 +0100

    Add files via upload

---
+++ GATE 1/TaskOnes.java
@@ -0,0 +1,29 @@
+public class TaskOne{
+public static void main(String [] args){
+
+/*int count = 1;
+
+System.out.println("count");
+*/
+
+for(; count <= 10; count++)
+{
+
+
+if(number > 10);
+    count = -1;
+s
+
+
+
+  { System.out.println("count");
+
+
+         }
+
+       }
+
+    }
+
+  }                                                                                           
+
